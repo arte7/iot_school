064c3fb8bea7eedffa817780d6e798dd6013f49a
Luka Ehlers <lehlers@netfonds.de>
2018-05-16 15:10:31 +0200

    aufteilen der lese und schreibfunktion
    400 beim senden an den spam chat

--- useData/readChat.py
+++ useData/readChat.py
@@ -1,10 +1,12 @@
 import requests
 import time
 import thingspeak
+import json
 
 
 accessToken = "NmFjOWViZWYtMTZjMy00Y2UxLWI3YmMtMTVkMzg2OTNhM2YyZmZiN2NhMGQtZTc2"
-roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODMzYzhiYTAtNTc1NS0xMWU4LTk1NDMtN2QxYzA5ZTViN2Ux"
+dataroomId = "Y2lzY29zcGFyazovL3VzL1JPT00vNGUyNjE1MjAtNTkwNC0xMWU4LWIwYTEtNmI0Y2M0NzNkZDVj"
+chatroomId = "Y2lzY29zcGFyazovL3VzL1JPT00vODMzYzhiYTAtNTc1NS0xMWU4LTk1NDMtN2QxYzA5ZTViN2Ux"
 url = "https://api.ciscospark.com/v1"
 headers = {"Authorization": "Bearer " + accessToken, "Content-Type": "application/json; charset=utf-8"}
 
@@ -16,7 +18,7 @@ def get_messages():
     oldresp = {}
 
     while status == 200:
-        resp = requests.get(url + "/messages?roomId=" + roomId + "&max=1", headers=headers, verify=False)
+        resp = requests.get(url + "/messages?roomId=" + dataroomId + "&max=1", headers=headers, verify=False)
 
         status = resp.status_code
         print("Request status: " + str(status) + "\n")
@@ -43,21 +45,20 @@ def get_messages():
 def write_to_db(obj):
 
     # holen der daten aus dem String
-    text = str(obj['text']).split()
-
-    data = '{"text": "Die Daten '+obj['text']+' wurden übermittlet", "roomId": "'+roomId+'"}'
+    text = json.loads(obj['text'])
+    data = '{"text": "Die Daten Temperatur: ' + str(text['Temperature']) + '°C  und Luftfeuchtigkeit: ' \
+           + str(text['Humidity']) + '% wurden übermittlet", "roomId": "'+chatroomId+'"}'
 
     channel_id = 496768
     write_key = "PZVVZFO8R5YJY0D7"
     read_key = "MO17791BOVEODHFV"
 
     channel = thingspeak.Channel(id=channel_id, write_key=write_key, api_key=read_key)
-    tsresponse = channel.update({'field1': text[1], 'field2': text[3]})
+    tsresponse = channel.update({'field1': text['Temperature'], 'field2': text['Humidity']})
 
     read = channel.get({})
     print("Read: ", read)
-
-
+    print(data)
     iswritten = requests.post(url+"/messages", data=data, headers=headers)
 
     print(iswritten.status_code)
